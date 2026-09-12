297d9cf6a30289124e3f0a4779d02430b5985049
Shaarav Dabas <shaarav2077@gmail.com>
2026-09-13 03:19:13 +0530

    Completed Python fundamentals and input function

---
+++ hello.py
@@ -0,0 +1,102 @@
+print("hello world ")
+print(24)
+print(24+8)
+name="shaarav" 
+print(name)
+height="6'0"
+print(height)
+print("my name is:", name)
+age=18
+age2=age 
+print("my age is:", age2)
+name1=("shaarav")
+print(name1)
+a=10
+b=20
+# print(a+b)
+print("my name is shaarav" ,"and my age is 18")
+# arthmetic operations (+,-,*,/,%,**)
+a=8
+b=24
+sum=a+b
+print(sum)
+print(type(age))
+print(type(name))
+print(a*b)
+print(b/a)
+print(b%a)# remiander
+print(a**b)#a^b
+# realtional operators (==, !=, <, >, <=, >=)
+print(a==b)
+print(type(a==b))
+#assignment operators (=, +=, -=, *=, /=, %=)
+num=10
+num+=10
+print("num:",num)
+num**=10
+print("num:",num)
+#logical operators (and, or, not)
+a=50
+b=20
+print(not(a>b))
+print(not(a<b))
+print(type(not(a>b)))
+print(type(not(a<b)))
+print(type(a))
+print(type("num"))
+print(type("num:"))
+print(type(a%b))
+val1=False
+val2=False
+print("and operator:",val1 and val2)
+print("OR operator:",val1 or val2)
+print("OR operator:",a==b or a>b)
+print("and operator:",a==b and a>b)
+print("and operator:",a**b and a%b)
+#type conversion
+a=10
+b=20.5
+print(type(a))
+sum=a+b
+print(sum)
+a=float("10")
+b=20.5
+sum=a+b
+print(sum)
+a=int("10")
+b=20.5
+sum=a+b
+print(sum)
+a=float("10")
+b=20 
+sum=a+b
+print(sum)
+a=3.14 
+a=str(a)
+print(type(a))
+#input functions
+name=input("enter your name:")
+age=int(input("enter your age:"))
+marks=float(input("enter your marks:"))
+print("my name is:",name)
+print("my age is:",age)
+print("my marks is:",marks)
+print(type(name))
+print(type(age))
+print(type(marks))
+val1=int(input("enter first number:"))
+val2=int(input("enter second number:"))
+sum=val1+val2
+print("sum of two numbers is:",sum)
+val1=int(input("first side of reactangle:"))
+val2=int(input("second side of reactangle:"))
+area=val1*val2
+print("area of rectangle is:",area)
+val1=float(input("side of square:"))
+print("area of sqaure",val1*val1)
+val1=float(input("first number:"))
+val2=float(input("second number:"))
+print("avg of values is:",val1+val2/2)
+val1=int(input("enter first number:"))
+val2=int(input("enter second number:"))
+print(val1>=val2)
